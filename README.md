dba7b797f2648ad6c7ca8da74a458d832616826e
Luke Stand <92410656+LukeStand@users.noreply.github.com>
2025-08-12 10:34:18 -0700

    added check for winner logic. need to make look nice. See read me for thought process on winner.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # odin-ticTacToe
 playyer 1 wil always be x and go first.
+checking for win we will loop through the whole array checking indexs that are stored in the winning array.
+
+there is a more efficient way using variables to count the lines and total plays. Game would end when total == 9, 
+or one of the winning line variables == 3 or -3. however this way of doing the problem adds more lines of code 
+which mak ethe code harder to read.
--- main.js
+++ main.js
@@ -28,8 +28,8 @@ function displayCell(currentCell){
 }
 function updateTurnDisplay(player)
 {
-    const span = document.querySelector("#current-player");
-    span.textContent = `${player.name}, marker: ${player.maker}`
+    const span = document.querySelector("#current-turn");
+    span.textContent = `Current Turn: ${player.name}, Marker: ${player.marker}`
 }
 //create a player
 function createPlayer(name,marker){
@@ -39,17 +39,21 @@ function createPlayer(name,marker){
 function createBoard()
 {
     displayBoard();
-    const gameBoard = [[0,0,0],[0,0,0],[0,0,0]];
-    playSpace = function (x,y,player)
+    const gameBoard = [0,0,0,0,0,0,0,0,0];
+    let playCount =0;
+    const winningLines = [[0,1,2],[3,4,5],[6,7,8],//winnning rows
+                        [0,3,6],[1,4,7],[2,5,8], // winning cols
+                        [0,4,8],[2,4,6]]; //winning diagnoals
+    const playSpace = function (index,player)
     {
-        if(gameBoard[x][y] == 0)
+        if(gameBoard[index] == 0)
         {
-            gameBoard[x][y] = player.marker;
+            gameBoard[index] = player.marker;
         }
     }
-    isEmpty = function (x,y)
+   const isEmpty = function (index)
     {
-        if(gameBoard[x][y] == 0)
+        if(gameBoard[index] == 0)
         {
             return true;
         }
@@ -57,9 +61,47 @@ function createBoard()
             return false;
         }
     }
-    return{playSpace, isEmpty};
+    const incrementPlayCount = function(){
+        playCount++;
+    } 
+    const getPlayCount = function(){
+        return playCount;
+    }
+    const getSpace = function(index){
+        return gameBoard[index];
+    }
+
+    return{playSpace, isEmpty,incrementPlayCount,getPlayCount,getSpace,winningLines};
 }
-function initiateAllObjects(name1,name2){
+function checkForWin()
+{
+    
+    const player = gameObj.players[gameObj.getCurrentPlayerIndex()];
+    console.log("in check for winner!" + player.marker)
+    for(let i = 0; i < 8;i++)
+    {
+        let a = gameObj.board.winningLines[i][0];
+        let b = gameObj.board.winningLines[i][1];
+        let c = gameObj.board.winningLines[i][2];
+        if(gameObj.board.getSpace(a) == player.marker && gameObj.board.getSpace(b) == player.marker && gameObj.board.getSpace(c) == player.marker)
+        {
+            console.log("winner!")
+            endGame(player);
+        }
+    }
+}
+function endGame(player)
+{
+    const dialog = document.querySelector("#myModal");
+    const cells = document.querySelectorAll(".grid-cell");
+    cells.forEach(cell => {
+        cell.remove();
+    });
+    dialog.textContent = `Winner is ${player.name}!!`
+    dialog.showModal();
+}
+function initiateAllObjects(name1,name2)
+{
     const board = createBoard();
     const player1 = createPlayer(name1,"X");
     const player2 = createPlayer(name2,"O");
@@ -74,26 +116,28 @@ function initiateAllObjects(name1,name2){
     {
         return currentPlayerIndex;
     };
-
-  return { players, board, nextTurn, getCurrentPlayerIndex };
+  return { players, board, nextTurn, getCurrentPlayerIndex, checkForWin, checkForWin};
 }
 
 //start the game
 function startGame(name1,name2)
 {
     gameObj = initiateAllObjects(name1,name2);
+    updateTurnDisplay(gameObj.players[gameObj.getCurrentPlayerIndex()]);
 }
 const grid =  document.querySelector(".grid-container");
 grid.addEventListener("click",function(e)
 {
     
     const currentCell = e.target;
-    const x = currentCell.dataset.x;
-    const y =currentCell.dataset.y;
-    if(gameObj.board.isEmpty(x,y))
+    const x = parseInt(currentCell.dataset.x);
+    const y =parseInt(currentCell.dataset.y);
+    const index = (x*3 +y);
+    if(gameObj.board.isEmpty(index))
     {
-        gameObj.board.playSpace(x,y,gameObj.players[gameObj.getCurrentPlayerIndex()]);
+        gameObj.board.playSpace(index,gameObj.players[gameObj.getCurrentPlayerIndex()]);
         displayCell(currentCell);
+        checkForWin();
         gameObj.nextTurn();
         updateTurnDisplay(gameObj.players[gameObj.getCurrentPlayerIndex()]);
     }
